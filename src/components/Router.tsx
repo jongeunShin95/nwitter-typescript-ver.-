8b10f1e5e61a3809734d95967fb09accd22f50de
jongeunShin95 <jongeunshin95@gmail.com>
2021-10-08 00:00:27 +0900

    Update 2/7/Log Out

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Auth from '../routes/Auth';
 import Home from '../routes/Home';
 import { User } from 'firebase';
+import Navigation from './Navigation';
+import Profile from '../routes/Profile';
 
 type RouterProps = {
     isLoggedIn: boolean | null
@@ -11,13 +13,25 @@ type RouterProps = {
 export default ({ isLoggedIn }: RouterProps) => {
     return (
         <Router>
+            { isLoggedIn && <Navigation /> }
             <Switch>
                 { isLoggedIn ?
                     <>
                         <Route exact path="/">
                             <Home />
                         </Route>
-                    </> : <Route exact path="/"><Auth /></Route>
+                        <Route exact path="/profile">
+                            <Profile />
+                        </Route>
+                        <Redirect from="*" to="/" />
+                    </> : (
+                        <>
+                            <Route exact path="/">
+                                <Auth />
+                            </Route>
+                            <Redirect from="*" to="/" />
+                        </>
+                    )
                 }
             </Switch>
         </Router>
