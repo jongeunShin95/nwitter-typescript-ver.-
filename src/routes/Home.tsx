cf6393ce7c218b0b36ccc5bb4a1d940ea445dac3
jongeunShin95 <jongeunshin95@gmail.com>
2021-10-10 22:39:26 +0900

    Update 3/0/Form and Database Setup

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,3 +1,25 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 
-export default () => <span>Home</span>;
+const Home = () => {
+    const [nweet, setNweet] = useState('');
+
+    const onSubmit = (e: FormEvent) => {
+        e.preventDefault();
+    }
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { target: { value } } = e;
+        setNweet(value);
+    }
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input value={nweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
+                <input type="submit" value="Nweet" /> 
+            </form>
+        </div>
+    );
+};
+
+export default Home;
